a5f3564a95d6750a80546d7db71a4a9c19115f5c
rebeccamrfoster <rebeccamrfoster@gmail.com>
2022-01-09 09:38:24 -0500

    Define handler for get request, with commented code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    // .status is function that sets status code
+    // .json is function that sends json response
+    res.status(200).json({ "hello": "world" })
 }
